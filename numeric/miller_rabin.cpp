11147ae7fc7a3923f47261ffbc73e8f654b62c15
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-03-20 00:20:50 +0900

    miller_rabin

--- numeric/miller_rabin.cpp
+++ numeric/miller_rabin.cpp
@@ -1,19 +1,20 @@
 struct miller_rabin {
+    using i64 = int64_t;
     using u64 = uint64_t;
-    using u128 = __uint128_t;
 
     bool is_prime(u64 n) {
+        using namespace modular;
         if (n < 2) return false;
         if (n == 2 || n == 3) return true;
         if (n % 6 != 1 && n % 6 != 5) return false;
 
-        const auto& base = n > 4759123141ULL ? base_large : base_small;
+        const auto& base = n < 4759123141ULL ? base_small : base_large;
         const int s = __builtin_ctzll(n - 1);
         const u64 d = n >> s;
 
         for (const auto& b : base) {
             if (b >= n) break;
-            if (not_prime(b, s, d, n)) return false;
+            if (check_composite(n, b, d, s)) return false;
         }
         return true;
     }
@@ -22,21 +23,13 @@ private:
     const vector<u64> base_small = {2, 7, 61},
                       base_large = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
 
-    u128 pow(u128 a, u64 p, u64 m) {
-        u128 r = 1;
-        for (; p; p >>= 1) {
-            if (p & 1) r = r * a % m;
-            a = a * a % m;
-        }
-        return r;
-    }
-    bool not_prime(u64 a, int s, u64 d, u64 n) {
-        u128 x = pow(a, d, n);
-        if (x == 1) return false;
-        for (int r = 0; r < s; ++r) {
+    bool check_composite(u64 n, u64 x, u64 d, int s) {
+        x = modular::pow(x, d, n);
+        if (x == 1 || x == n - 1) return false;
+        while (--s) {
+            x = modular::mul(x, x, n);
             if (x == n - 1) return false;
-            x = x * x % n;
         }
         return true;
-    }
+    };
 };
