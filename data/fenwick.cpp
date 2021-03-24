08f34bb9ce2c2b0e6b6dd5348dd9f56cc16311cf
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-03-24 15:52:19 +0900

    segtree

---
+++ data/fenwick.cpp
@@ -0,0 +1,21 @@
+template <typename T>
+struct BIT {
+    BIT(int _n) : n(_n), tree(_n + 1) {}
+    void update(int i, T val) {
+        for (++i; i <= n; i += i & -i)
+            tree[i] += val;
+    }
+    T query(int i) {
+        T ret = 0;
+        for (; i; i &= i - 1)
+            ret += tree[i];
+        return ret;
+    }
+    T query(int l, int r) {
+        return query(r) - query(l);
+    }
+
+private:
+    const int n;
+    vector<T> tree;
+};
---
+++ data/seg_lazy.cpp
@@ -0,0 +1,81 @@
+template <typename node_t, typename tag_t>
+struct lazy_seg {
+    lazy_seg(int _n) : n(_n), height(__lg(_n - 1) + 1), size(1 << height),
+                       tree(size << 1, e), lazy(size, off) {}
+
+    node_t& operator[](int i) { return tree[size + i]; }
+    void build() {
+        for (int i = size - 1; i; --i)
+            pull(i);
+    }
+    void apply(int l, int r, tag_t f) {
+        apply(l, r, f, 0, size, 1);
+    }
+    node_t prod(int l, int r) {
+        return prod(l, r, 0, size, 1);
+    }
+
+private:
+    const node_t e = node_t {};
+    const tag_t off = tag_t {};
+    const int n, height, size;
+    vector<node_t> tree;
+    vector<tag_t> lazy;
+
+    inline int get_index(node_t& node) { return &node - tree.data(); }
+    inline int get_depth(node_t& node) { return __lg(get_index(node)); }
+    inline int get_height(node_t& node) { return height - get_depth(node); }
+    inline int get_length(node_t& node) { return 1 << get_height(node); }
+    inline int get_first(node_t& node) {
+        int idx = get_index(node);
+        int dep = __lg(idx);
+        int len = 1 << height - dep;
+        return len * (idx ^ 1 << dep);
+    }
+
+#define lson (i << 1)
+#define rson (i << 1 | 1)
+    void apply(int ql, int qr, tag_t f, int l, int r, int i) {
+        if (qr <= l || r <= ql) return;
+        if (ql <= l && r <= qr) {
+            all_apply(i, f);
+            return;
+        }
+        if (lazy[i] != off) push(i);
+
+        const auto m = l + r >> 1;
+        apply(ql, qr, f, l, m, lson), apply(ql, qr, f, m, r, rson);
+        pull(i);
+    }
+    node_t prod(int ql, int qr, int l, int r, int i) {
+        if (qr <= l || r <= ql) return e;
+        if (ql <= l && r <= qr) return tree[i];
+        if (lazy[i] != off) push(i);
+
+        const auto m = l + r >> 1;
+        return op(prod(ql, qr, l, m, lson), prod(ql, qr, m, r, rson));
+    }
+    void pull(int i) {
+        tree[i] = op(tree[lson], tree[rson]);
+    }
+    void push(int i) {
+        all_apply(lson, lazy[i]);
+        all_apply(rson, lazy[i]);
+        lazy[i] = off;
+    }
+    void all_apply(int i, tag_t f) {
+        mapping(tree[i], f);
+        if (i < size) composition(lazy[i], f);
+    }
+    node_t op(node_t lhs, node_t rhs) {
+        assert(true);
+    }
+    void mapping(node_t& node, tag_t f) {
+        assert(true);
+    }
+    void composition(tag_t& tag, tag_t f) {
+        assert(true);
+    }
+#undef lson
+#undef rson
+};
