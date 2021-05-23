2dc9cf9c2aaa9e8dba5b541dc6563e32dc3c8002
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-05-23 13:10:59 +0900

    unite `Miller-Rabin` & `Pollard's rho`

---
+++ math/primality_test.cpp
@@ -0,0 +1,70 @@
+/**
+ * @brief 
+ *      Miller-Rabin & Pollard's rho
+ */
+class primality_test {
+    using num = unsigned long long;
+
+public:
+    bool is_prime(num n) {
+        if (n < 2) return false;
+        if (n == 2 || n == 3) return true;
+        if (n % 6 != 1 && n % 6 != 5) return false;
+
+        const auto& base = n < 4759123141ULL ? base_small : base_large;
+        const int s = __builtin_ctzll(n - 1);
+        const num d = n >> s;
+
+        for (const auto& b : base) {
+            if (b >= n) break;
+            if (check_composite(n, b, d, s)) return false;
+        }
+        return true;
+    }
+    vector<num> factorize(num n) {
+        if (n == 1) return {};
+        if (is_prime(n)) return {n};
+
+        const num x = pollard(n);
+        auto l = factorize(x), r = factorize(n / x);
+
+        decltype(l) ret(l.size() + r.size());
+        merge(l.begin(), l.end(), r.begin(), r.end(), ret.begin());
+        return ret;
+    }
+
+private:
+    const vector<num> base_small = {2, 7, 61},
+                      base_large = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
+
+    num pow_mod(num a, num p, num m) {
+        num ret = 1;
+        for (; p; p >>= 1) {
+            if (p & 1) ret = mul_mod(ret, a, m);
+            a = mul_mod(a, a, m);
+        }
+        return ret;
+    }
+    num mul_mod(num a, num b, num m) {
+        int64_t ret = a * b - m * num(1.L / m * a * b);
+        return ret + m * (ret < 0) - m * (ret >= int64_t(m));
+    }
+    bool check_composite(num n, num x, num d, int s) {
+        x = pow_mod(x, d, n);
+        if (x == 1 || x == n - 1) return false;
+        while (--s) {
+            x = mul_mod(x, x, n);
+            if (x == n - 1) return false;
+        }
+        return true;
+    };
+    num pollard(num n) {
+        auto f = [&](num x) { return mul_mod(x, x, n) + 1; };
+        num x = 0, y = 0, prd = 2, i = 1, q;
+        for (int t = 30; t++ % 40 || gcd(prd, n) == 1; x = f(x), y = f(f(y))) {
+            if (x == y) x = ++i, y = f(x);
+            if ((q = mul_mod(prd, x > y ? x - y : y - x, n))) prd = q;
+        }
+        return gcd(prd, n);
+    }
+};
