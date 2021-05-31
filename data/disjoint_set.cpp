c030a5e6febcb6bffab11edb0949de2d5f166a16
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-05-31 17:27:37 +0900

    add `assert`

--- data/disjoint_set.cpp
+++ data/disjoint_set.cpp
@@ -3,15 +3,15 @@
  *      Disjoint Set Union (a.k.a Union-Find)
  */
 class disjoint_set {
-    vector<int> par;
-
 public:
-    disjoint_set(int n) : par(n, -1) {}
+    disjoint_set(int _n) : n(_n), par(n, -1) {}
 
     int find(int u) {
+        assert(0 <= u and u < n);
         return par[u] < 0 ? u : par[u] = find(par[u]);
     }
     bool unite(int u, int v) {
+        assert(0 <= u and u < n and 0 <= v and v < n);
         u = find(u), v = find(v);
         if (u == v) return false;
 
@@ -20,6 +20,16 @@ public:
         par[v] = u;
         return true;
     }
-    int size_of(int u) { return -par[find(u)]; }
-    bool is_same(int u, int v) { return find(u) == find(v); }
+    int size_of(int u) {
+        assert(0 <= u and u < n);
+        return -par[find(u)];
+    }
+    bool is_same(int u, int v) {
+        assert(0 <= u and u < n and 0 <= v and v < n);
+        return find(u) == find(v);
+    }
+
+private:
+    const int n;
+    vector<int> par;
 };
