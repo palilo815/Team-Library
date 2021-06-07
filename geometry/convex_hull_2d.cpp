8df04edcf27194debc163255f7b9836cd9cd0451
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-06-07 15:19:36 +0900

    all functions are tested

---
+++ geometry/convex_hull_2d.cpp
@@ -0,0 +1,21 @@
+/**
+ * @link 
+ *      https://github.com/stjepang/snippets/blob/master/convex_hull.cpp
+ * @warning
+ *      points on the edge of the hull will be ignored
+ * @return
+ *      points of the hull in ccw order
+ */
+template <typename T>
+vector<point2D<T>> convex_hull(vector<point2D<T>> pts) {
+    if (pts.size() <= 1) return pts;
+    sort(pts.begin(), pts.end());
+    vector<point2D<T>> hull(pts.size() + 1);
+    int s = 0, t = 0;
+    for (int it = 2; it--; s = --t, reverse(pts.begin(), pts.end()))
+        for (const auto& p : pts) {
+            while (t >= s + 2 && hull[t - 2].cross(hull[t - 1], p) <= 0) t--;
+            hull[t++] = p;
+        }
+    return {hull.begin(), hull.begin() + t - (t == 2 && hull[0] == hull[1])};
+}
---
+++ geometry/point_2d.cpp
@@ -0,0 +1,55 @@
+/**
+ * @link
+ *      https://github.com/kth-competitive-programming/kactl/blob/main/content/geometry/Point.h 
+ * @warning
+ *      integer overflow in `dot`, `cross`, `dist2`
+ *      just use `long long`. NOT `int`
+ */
+template <typename T>
+struct point2D {
+    T x, y;
+    point2D() : x(0), y(0) {}
+    point2D(T _x, T _y) : x(_x), y(_y) {}
+    template <typename U>
+    explicit point2D(const point2D<U>& p) : x(p.x), y(p.y) {}
+
+    using P = point2D;
+
+    bool operator<(P p) const { return tie(x, y) < tie(p.x, p.y); }
+    bool operator==(P p) const { return tie(x, y) == tie(p.x, p.y); }
+    bool operator!=(P p) const { return tie(x, y) != tie(p.x, p.y); }
+
+    friend P operator+(const P& a, const P& b) { return P(a.x + b.x, a.y + b.y); }
+    friend P operator-(const P& a, const P& b) { return P(a.x - b.x, a.y - b.y); }
+    friend P operator*(const P& a, const T& scala) { return P(a.x * scala, a.y * scala); }
+    friend P operator*(const T& scala, const P& a) { return P(scala * a.x, scala * a.y); }
+    friend P operator/(const P& a, const T& scala) { return P(a.x / scala, a.y / scala); }
+
+    friend ostream& operator<<(ostream& o, const P& p) { return o << '(' << p.x << ", " << p.y << ')'; }
+    friend istream& operator>>(istream& i, P& p) { return i >> p.x >> p.y; }
+
+    T dot(const P& p) const { return x * p.x + y * p.y; }
+    T cross(const P& p) const { return x * p.y - y * p.x; }
+    T cross(const P& a, const P& b) const { return (a - *this).cross(b - *this); }
+
+    T dist2() const { return x * x + y * y; }
+    double dist() const { return sqrt(dist2()); }
+
+    P conj() const { return P(x, -y); }
+    P perp_cw() const { return P(y, -x); }
+    P perp_ccw() const { return P(-y, x); }
+
+    P unit() const { return *this / dist(); }
+    P normal() const { return perp_ccw().unit(); }
+
+    P unit_int() const { return x || y ? *this / gcd(x, y) : *this; }
+    P normal_int() const { return perp_ccw().unit_int(); }
+
+    bool same_dir(const P& p) const { return cross(p) == 0 && dot(p) > 0; }
+    // angle to x-axis in interval (-pi, pi]
+    double angle() const { return atan2(y, x); }
+    // returns point rotated 'a' radians ccw around the origin
+    P rotate(double radian) const {
+        return P(x * cos(radian) - y * sin(radian), x * sin(radian) + y * cos(radian));
+    }
+};
