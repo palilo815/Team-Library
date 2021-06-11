de2a7ed1e0961ac7990f82733a2b53b016b08a11
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-06-11 15:48:59 +0900

    decrease precision error

--- geometry/intersected.cpp
+++ geometry/intersected.cpp
@@ -3,14 +3,12 @@
  *      palilo
  * @brief
  *      determine whether the two objects are intersected
- * @warning
- *      precision in `sign(cross)`. it can be greater than `EPS`
  */
 namespace geo::intersected {
 template <typename T>
 bool segment_segment(const pair<point2D<T>, point2D<T>>& u, const pair<point2D<T>, point2D<T>>& v) {
-    if (sign(u.first.cross(u.second, v.first)) * sign(u.first.cross(u.second, v.second)) < 0 &&
-        sign(v.first.cross(v.second, u.first)) * sign(v.first.cross(v.second, u.second)) < 0)
+    if (u.first.side_of(v.first, v.second) * u.second.side_of(v.first, v.second) < 0 &&
+        v.first.side_of(u.first, u.second) * v.second.side_of(u.first, u.second) < 0)
         return true;
     return point_in::segment(u.first, v) || point_in::segment(u.second, v) ||
            point_in::segment(v.first, u) || point_in::segment(v.second, u);
