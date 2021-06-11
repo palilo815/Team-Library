36fbfccd139b2732c63cbcde2f884c10451f662f
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-06-11 14:04:15 +0900

    tested

--- geometry/point_in.cpp
+++ geometry/point_in.cpp
@@ -18,7 +18,7 @@ bool segment(const point2D<T>& p, const pair<point2D<T>, point2D<T>>& obj) {
 
 template <typename T>
 bool hull(const point2D<T>& p, const vector<point2D<T>>& obj, bool boundary) {
-    if (obj.size() < 3) return boundary && geo::point_in::segment(p, pair(obj.front(), obj.back()));
+    if (obj.size() < 3) return boundary && point_in::segment(p, pair(obj.front(), obj.back()));
 
     int lo = 1, hi = obj.size() - 1;
     if (p.side_of(obj[0], obj[lo]) <= -boundary || p.side_of(obj[0], obj[hi]) >= boundary)
@@ -29,5 +29,16 @@ bool hull(const point2D<T>& p, const vector<point2D<T>>& obj, bool boundary) {
         (p.side_of(obj[0], obj[mid]) == 1 ? lo : hi) = mid;
     }
     return p.side_of(obj[lo], obj[hi]) > -boundary;
+}
+
+template <typename T>
+bool polygon(const point2D<T>& p, const vector<point2D<T>>& obj, bool boundary) {
+    bool ret = false;
+    for (int i = 0; i < int(obj.size()); ++i) {
+        const auto s = obj[i], e = obj[i == int(obj.size()) - 1 ? 0 : i + 1];
+        if (point_in::segment(p, pair(s, e))) return boundary;
+        ret ^= ((p.y < s.y) - (p.y < e.y)) * p.cross(s, e) > 0;
+    }
+    return ret;
 }
 }; // namespace geo::point_in
