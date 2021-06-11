4caad6305aa53a9c3716e7d8c41d60b9d2200fed
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-06-11 13:43:59 +0900

    change some UI

--- geometry/point_2d.cpp
+++ geometry/point_2d.cpp
@@ -1,14 +1,29 @@
 /**
  * @link
- *      https://github.com/kth-competitive-programming/kactl/blob/main/content/geometry/Point.h 
+ *      https://github.com/kth-competitive-programming/kactl/blob/main/content/geometry/Point.h
+ * @brief
+ *      some basic functions and struct `point2D`
  * @warning
  *      integer overflow in `dot`, `cross`, `dist2`
  *      just use `long long`. NOT `int`
+ *      choose some proper `EPS` value
  */
+
+/* basics */
+namespace geo {
+#define EPS 1e-8
+template <typename T, enable_if_t<is_integral<T>::value, bool> = true>
+int sign(T x) { return (x > 0) - (x < 0); }
+template <typename T, enable_if_t<is_floating_point<T>::value, bool> = true>
+int sign(T x) { return (x > EPS) - (x < -EPS); }
+}; // namespace geo
+
+/* point2D */
+namespace geo {
 template <typename T>
 struct point2D {
     T x, y;
-    point2D() : x(0), y(0) {}
+    point2D() = default;
     point2D(T _x, T _y) : x(_x), y(_y) {}
     template <typename U>
     explicit point2D(const point2D<U>& p) : x(p.x), y(p.y) {}
@@ -21,9 +36,9 @@ struct point2D {
 
     friend P operator+(const P& a, const P& b) { return P(a.x + b.x, a.y + b.y); }
     friend P operator-(const P& a, const P& b) { return P(a.x - b.x, a.y - b.y); }
-    friend P operator*(const P& a, const T& scala) { return P(a.x * scala, a.y * scala); }
-    friend P operator*(const T& scala, const P& a) { return P(scala * a.x, scala * a.y); }
-    friend P operator/(const P& a, const T& scala) { return P(a.x / scala, a.y / scala); }
+    friend P operator*(const P& a, const T& scalar) { return P(a.x * scalar, a.y * scalar); }
+    friend P operator*(const T& scalar, const P& a) { return P(scalar * a.x, scalar * a.y); }
+    friend P operator/(const P& a, const T& scalar) { return P(a.x / scalar, a.y / scalar); }
 
     friend ostream& operator<<(ostream& o, const P& p) { return o << '(' << p.x << ", " << p.y << ')'; }
     friend istream& operator>>(istream& i, P& p) { return i >> p.x >> p.y; }
@@ -35,7 +50,6 @@ struct point2D {
     T dist2() const { return x * x + y * y; }
     double dist() const { return sqrt(dist2()); }
 
-    P conj() const { return P(x, -y); }
     P perp_cw() const { return P(y, -x); }
     P perp_ccw() const { return P(-y, x); }
 
@@ -46,20 +60,13 @@ struct point2D {
     P normal_int() const { return perp_ccw().unit_int(); }
 
     bool same_dir(const P& p) const { return cross(p) == 0 && dot(p) > 0; }
-    bool on_segment(const P& s, const P& e) const {
-        if constexpr (is_integral_v<T>)
-            return cross(s, e) == 0 && (s - *this).dot(e - *this) <= 0;
-        else
-            return cross(s, e) == 0 && (s - *this).dot(e - *this) <= 1e-9;
-    }
     int side_of(const P& s, const P& e) const {
         if constexpr (is_integral_v<T>) {
-            auto c = s.cross(e, *this);
-            return (c > 0) - (c < 0);
+            return sign(s.cross(e, *this));
         } else {
-            auto a = (e - s).cross(*this - s);
-            double l = (e - s).dist() * 1e-9;
-            return (a > l) - (a < -l);
+            auto res = (e - s).cross(*this - s);
+            double l = (e - s).dist() * EPS;
+            return (res > l) - (res < -l);
         }
     }
 
@@ -68,3 +75,4 @@ struct point2D {
         return P(x * cos(radian) - y * sin(radian), x * sin(radian) + y * cos(radian));
     }
 };
+}; // namespace geo
