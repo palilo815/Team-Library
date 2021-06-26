cfd896217c5a2a9c2779e987384c28b7f7e943ca
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-06-27 03:13:39 +0900

    discard mine :(

--- numeric/modint.cpp
+++ numeric/modint.cpp
@@ -1,110 +1,115 @@
 /**
- * using mint = mint<998244353>;
+ * @link https://github.com/ecnerwala/cp-book/blob/master/src/modnum.hpp
  */
-template <int _mod>
-struct mint {
-    mint() : v(0) {}
-    mint(int64_t _v) : v(int(_v % mod)) {
-        if (v < 0) v += mod;
+template <typename T>
+T mod_inv_in_range(T a, T m) {
+    T x = a, y = m;
+    T vx = 1, vy = 0;
+    while (x) {
+        T k = y / x;
+        y %= x;
+        vy -= k * vx;
+        std::swap(x, y);
+        std::swap(vx, vy);
     }
+    assert(y == 1);
+    return vy < 0 ? m + vy : vy;
+}
 
-    mint inv() const {
-        if (is_prime) {
-            assert(v);
-            return pow(mod - 2);
-        } else {
-            int m0 = 0, m1 = 1, a = v, m = mod;
-            while (a != 0) {
-                int t = m / a;
-                m -= t * a;
-                swap(a, m);
-                m0 -= t * m1;
-                swap(m0, m1);
-            }
-            assert(m == 1);
-            mint res;
-            res.v = m0 < 0 ? m0 + mod : m0;
-            return res;
-        }
-    }
-    mint neg() const {
-        mint res;
-        res.v = v ? mod - v : 0;
-        return res;
-    }
-    mint pow(int64_t p) const {
-        assert(p >= 0);
-        int64_t x = v, r = 1;
-        while (p) {
-            if (p & 1) r = r * x % mod;
-            p >>= 1;
-            x = x * x % mod;
-        }
-        mint res;
-        res.v = r;
-        return res;
-    }
+template <typename T>
+T mod_inv(T a, T m) {
+    a %= m;
+    a = a < 0 ? a + m : a;
+    return mod_inv_in_range(a, m);
+}
 
-    operator int() const { return v; }
+template <int MOD_>
+struct modnum {
+    static constexpr int MOD = MOD_;
+    static_assert(MOD_ > 0, "MOD must be positive");
 
-    mint operator-() const { return neg(); }
-    mint operator+() const { return mint(*this); }
+private:
+    using ll = long long;
 
-    mint& operator++() {
-        if (++v == mod) v = 0;
-        return *this;
-    }
-    mint& operator--() {
-        v = v ? v - 1 : mod - 1;
-        return *this;
-    }
-    mint& operator+=(const mint& o) {
-        v -= mod - o.v;
-        if (v < 0) v += mod;
-        return *this;
-    }
-    mint& operator-=(const mint& o) {
-        v -= o.v;
-        if (v < 0) v += mod;
-        return *this;
-    }
-    mint& operator*=(const mint& o) {
-        v = int64_t(v) * o.v % mod;
-        return *this;
-    }
-    mint& operator/=(const mint& o) {
-        return *this *= o.inv();
-    }
+    int v;
 
-    friend ostream& operator<<(ostream& out, const mint& n) { return out << int(n); }
-    friend istream& operator>>(istream& in, mint& n) {
-        int64_t _v;
-        in >> _v;
-        n = mint(_v);
+public:
+    modnum() : v(0) {}
+    modnum(ll v_) : v(int(v_ % MOD)) {
+        if (v < 0) v += MOD;
+    }
+    explicit operator int() const { return v; }
+    friend std::ostream& operator<<(std::ostream& out, const modnum& n) { return out << int(n); }
+    friend std::istream& operator>>(std::istream& in, modnum& n) {
+        ll v_;
+        in >> v_;
+        n = modnum(v_);
         return in;
     }
 
-    friend mint operator++(mint& a, int) {
-        mint r = a;
+    friend bool operator==(const modnum& a, const modnum& b) { return a.v == b.v; }
+    friend bool operator!=(const modnum& a, const modnum& b) { return a.v != b.v; }
+
+    modnum inv() const {
+        modnum res;
+        res.v = mod_inv_in_range(v, MOD);
+        return res;
+    }
+    friend modnum inv(const modnum& m) { return m.inv(); }
+    modnum neg() const {
+        modnum res;
+        res.v = v ? MOD - v : 0;
+        return res;
+    }
+    friend modnum neg(const modnum& m) { return m.neg(); }
+
+    modnum operator-() const {
+        return neg();
+    }
+    modnum operator+() const {
+        return modnum(*this);
+    }
+
+    modnum& operator++() {
+        v++;
+        if (v == MOD) v = 0;
+        return *this;
+    }
+    modnum& operator--() {
+        if (v == 0) v = MOD;
+        v--;
+        return *this;
+    }
+    modnum& operator+=(const modnum& o) {
+        v -= MOD - o.v;
+        v = (v < 0) ? v + MOD : v;
+        return *this;
+    }
+    modnum& operator-=(const modnum& o) {
+        v -= o.v;
+        v = (v < 0) ? v + MOD : v;
+        return *this;
+    }
+    modnum& operator*=(const modnum& o) {
+        v = int(ll(v) * ll(o.v) % MOD);
+        return *this;
+    }
+    modnum& operator/=(const modnum& o) {
+        return *this *= o.inv();
+    }
+
+    friend modnum operator++(modnum& a, int) {
+        modnum r = a;
         ++a;
         return r;
     }
-    friend mint operator--(mint& a, int) {
-        mint r = a;
+    friend modnum operator--(modnum& a, int) {
+        modnum r = a;
         --a;
         return r;
     }
-    friend mint operator+(const mint& a, const mint& b) { return mint(a) += b; }
-    friend mint operator-(const mint& a, const mint& b) { return mint(a) -= b; }
-    friend mint operator*(const mint& a, const mint& b) { return mint(a) *= b; }
-    friend mint operator/(const mint& a, const mint& b) { return mint(a) /= b; }
-    friend bool operator==(const mint& a, const mint& b) { return a.v == b.v; }
-    friend bool operator!=(const mint& a, const mint& b) { return a.v != b.v; }
-
-private:
-    static constexpr int mod = _mod;
-    static constexpr bool is_prime = mod == 998'244'353 || mod == 1'000'000'007;
-    static_assert(_mod > 0);
-
-    int v;
+    friend modnum operator+(const modnum& a, const modnum& b) { return modnum(a) += b; }
+    friend modnum operator-(const modnum& a, const modnum& b) { return modnum(a) -= b; }
+    friend modnum operator*(const modnum& a, const modnum& b) { return modnum(a) *= b; }
+    friend modnum operator/(const modnum& a, const modnum& b) { return modnum(a) /= b; }
 };
