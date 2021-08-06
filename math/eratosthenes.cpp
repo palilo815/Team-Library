7c004ef4e1fe64959a2ed184cc329553f7e3195c
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-08-06 16:19:29 +0900

    flexible version (MAX is not a constexpr)

--- math/eratosthenes.cpp
+++ math/eratosthenes.cpp
@@ -1,45 +1,52 @@
 /**
- * @link
- *      https://github.com/kth-competitive-programming/kactl/blob/main/content/number-theory/FastEratosthenes.h
- *
- * @brief 
- *      faster Eratosthenes' sieve !!!!
- *      
- *      two methods to check x is prime
- *          1. is_prime[x] = true
- *          2. x in primes
+ * @link  https://github.com/kth-competitive-programming/kactl/blob/main/content/number-theory/FastEratosthenes.h
+ * @brief implement Eratosthenes' sieve
+ * @note  $O(MAX)$
  */
-template <int MAX>
-class eratosthenes {
-public:
-    bitset<MAX> is_prime;
-    vector<int> get_primes() {
-        const int s = round(sqrt(MAX)), r = MAX >> 1;
-
-        vector<int> primes = {2}, sieve(s + 1);
-        primes.reserve(MAX / log(MAX) * 1.1);
-
-        vector<pair<int, int>> cp;
-        for (int i = 3; i <= s; i += 2)
-            if (!sieve[i]) {
-                cp.emplace_back(i, (i * i) >> 1);
-                for (int j = i * i; j <= s; j += i << 1)
-                    sieve[j] = 1;
+vector<int> get_primes(int MAX) {
+    const int s = round(sqrt(MAX)), r = MAX >> 1;
+    vector<int> primes = {2}, sieve(s + 1);
+    primes.reserve(MAX / log(MAX) * 1.1);
+    vector<pair<int, int>> cp;
+    for (int i = 3; i <= s; i += 2) {
+        if (!sieve[i]) {
+            cp.emplace_back(i, (i * i) >> 1);
+            for (int j = i * i; j <= s; j += i << 1) {
+                sieve[j] = 1;
             }
-
-        for (int l = 1; l <= r; l += s) {
-            array<bool, s> block {};
-            for (auto& [p, idx] : cp)
-                for (int i = idx; i < s + l; idx = (i += p))
-                    block[i - l] = 1;
-
-            for (int i = 0, j = min(s, r - l); i < j; ++i)
-                if (!block[i])
-                    primes.emplace_back((l + i) << 1 | 1);
         }
-
-        for (const auto& i : primes)
-            is_prime[i] = true;
-        return primes;
     }
-};
+    vector<char> block(s);
+    for (int l = 1; l <= r; l += s) {
+        memset(block.data(), 0, s);
+        for (auto& [p, idx] : cp) {
+            for (int i = idx; i < s + l; idx = (i += p)) {
+                block[i - l] = 1;
+            }
+        }
+        for (int i = 0, j = min(s, r - l); i < j; ++i) {
+            if (!block[i]) {
+                primes.emplace_back((l + i) << 1 | 1);
+            }
+        }
+    }
+    return primes;
+}
+
+vector<int> get_factors(int MAX) {
+    vector<int> f(MAX), primes;
+    primes.reserve(MAX / log(MAX) * 1.1);
+    for (int i = 2; i <= MAX; ++i) {
+        if (f[i] == 0) {
+            f[i] = i;
+            primes.emplace_back(i);
+        }
+        for (const auto& p : primes) {
+            if (p > f[i] || i * p > MAX) {
+                break;
+            }
+            f[i * p] = p;
+        }
+    }
+    return f;
+}
