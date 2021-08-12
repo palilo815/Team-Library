45e193e83b8ce8b0bb120b4ea65d83ae036ff3af
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-08-13 00:48:01 +0900

    todo: range_frequency

--- data/wavelet_matrix.cpp
+++ data/wavelet_matrix.cpp
@@ -1,208 +1,129 @@
 /**
- * @link
- *      http://kmyk.github.io/competitive-programming-library/data_structure/wavelet_matrix.hpp
- * 
- * @brief
- *      Wavelet Matrix
- * 
- * @note 
- *      `rank(value, l, r)`
- *      	@return # of value in [l, r)
- *                  $ O(BITS) $
- *      
- *      `select(value, k)`
- *          @return index of the k-th occurence of value
- *                  $ O(BITS \log{\frac{n}{64})} $
- *      `select(value, k, l)`
- *          @return index of the k-th occurence of value in [l, n)
- * 
- *      `access(i)`
- *          @return i-th element
- *                  $ O(BITS) $
- * 
- *      `quantile(k, l, r)`
- *          @return k-th value in [l, r)
- *                  $ O(BITS) $
- * 
- *      `range_frequency(l, r, value_l, value_r)`
- *          @return # of values in [value_l, value_r) in range [l, r)
- *                  $ O(BITS) $
+ * @author  palilo
+ * @brief   wavelet_matrix & helper class bit_array for bit prefix sum
+ * @warning only non-negative integers are allowed in wavelet_matrix
+ *          but allowed in compressed_wavelet_matrix. it will perform coordinate compression
  */
 
-class fully_indexable_dictionary {
-    static constexpr int block_size = 64;
-    vector<uint64_t> block;
-    vector<int> rank_block;
+class bit_array {
+    using block_t = unsigned long long;
+
+    const size_t size;
+    vector<block_t> bit;
+    vector<int> pref;
 
 public:
-    int n;
-    fully_indexable_dictionary() = default;
+    bit_array(size_t n) : size((n + 63) >> 6), bit(size), pref(size + 1) {}
 
-    template <typename T>
-    fully_indexable_dictionary(const vector<T>& bits) : n(bits.size()) {
-        const int block_count = n / block_size + 1;
-
-        block.resize(block_count);
-        for (int i = 0; i < n; ++i)
-            if (bits[i])
-                block[i / block_size] |= 1ull << (i % block_size);
-
-        rank_block.resize(block_count);
-        for (int i = 1; i < block_count; ++i)
-            rank_block[i] = rank_block[i - 1] + __builtin_popcountll(block[i - 1]);
+    void set(size_t i, bool v) {
+        const auto block_id = i >> 6, bit_id = i & 63;
+        bit[block_id] = (bit[block_id] & ~(static_cast<block_t>(1) << bit_id)) | (static_cast<block_t>(v) << bit_id);
     }
-
-    int rank(bool value, int r) const {
-        assert(0 <= r and r <= n);
-        uint64_t mask = (1ull << (r % block_size)) - 1;
-        int rank_1 = rank_block[r / block_size] + __builtin_popcountll(block[r / block_size] & mask);
-        return value ? rank_1 : r - rank_1;
-    }
-    int rank(bool value, int l, int r) const {
-        assert(0 <= l and l <= r and r <= n);
-        return rank(value, r) - rank(value, l);
-    }
-
-    int select(bool value, int k) const {
-        if (k >= rank(value, n)) return n;
-
-        int lo = 0, hi = int(block.size()) - 1;
-        while (lo != hi) {
-            int mid = (lo + hi) >> 1;
-            (value ? rank_block[mid] : mid * block_size - rank_block[mid]) <= k ? lo = mid : hi = mid - 1;
-        }
-
-        lo *= block_size;
-        hi = min(n, lo + block_size);
-        while (lo != hi) {
-            int mid = (lo + hi) >> 1;
-            rank(value, mid) <= k ? lo = mid : hi = mid - 1;
-        }
-        return lo;
-    }
-    int select(bool value, int k, int l) const {
-        assert(0 <= l and l <= n);
-        return select(value, k + rank(value, l));
-    }
-
-    bool access(int i) const {
-        assert(0 <= i and i < n);
-        return block[i / block_size] >> (i % block_size) & 1;
-    }
-};
-
-template <int BITS>
-class wavelet_matrix {
-    const int n;
-    array<fully_indexable_dictionary, BITS> fid;
-    array<int, BITS> zero_count;
-
-public:
-    template <typename T>
-    wavelet_matrix(vector<T> data) : n(data.size()) {
-        for (const auto& x : data)
-            assert(0 <= x and x < (1ull << BITS));
-
-        vector<uint8_t> bits(n);
-        vector<T> next(n);
-
-        for (int k = BITS; k--;) {
-            int lo = 0, hi = n;
-            for (int i = 0; i < n; ++i) {
-                bits[i] = data[i] >> k & 1;
-                (bits[i] ? next[--hi] : next[lo++]) = data[i];
-            }
-
-            fid[k] = fully_indexable_dictionary(bits);
-            zero_count[k] = lo;
-            reverse(next.begin() + hi, next.end());
-            data.swap(next);
+    void build() {
+        for (size_t i = 0; i != size; ++i) {
+            pref[i + 1] = pref[i] + __builtin_popcountll(bit[i]);
         }
     }
-
-    int rank(uint64_t value, int l, int r) const {
-        assert(0 <= value and value < (1ull << BITS));
-        assert(0 <= l and l <= r and r <= n);
-
-        for (int k = BITS; k--;) {
-            bool p = value >> k & 1;
-            l = fid[k].rank(p, l) + p * zero_count[k];
-            r = fid[k].rank(p, r) + p * zero_count[k];
-        }
-        return r - l;
+    size_t rank(size_t r, bool value) const {
+        return value ? rank(r) : r - rank(r);
     }
-    int select(uint64_t value, int k) const {
-        assert(0 <= value and value < (1ull << BITS));
-        assert(0 <= k);
-
-        vector<int> l(BITS + 1), r(BITS + 1);
-        r[BITS] = fid[0].n;
-
-        for (int d = BITS; d--;) {
-            bool p = value >> d & 1;
-            l[d] = fid[d].rank(p, l[d + 1]) + p * zero_count[d];
-            r[d] = fid[d].rank(p, r[d + 1]) + p * zero_count[d];
-        }
-        if (r[0] - l[0] <= k) return fid[0].n;
-
-        for (int d = 0; d < BITS; ++d) {
-            bool p = value >> d & 1;
-            k = fid[d].select(p, k, l[d + 1]) - l[d + 1];
-        }
-        return k;
-    }
-    int select(uint64_t value, int k, int l) const {
-        return select(value, k + rank(value, l));
-    }
-    uint64_t access(int i) const {
-        assert(0 <= i and i < fid[0].n);
-        uint64_t acc = 0;
-        for (int k = BITS; k--;) {
-            bool p = fid[k].access(i);
-            acc |= uint64_t(p) << k;
-            i = fid[k].rank(p, i) + p * zero_count[k];
-        }
-        return acc;
-    }
-    uint64_t quantile(int k, int l, int r) {
-        assert(0 <= k);
-        assert(0 <= l and l <= r and r <= fid[0].n);
-        if (r - l <= k) return 1ull << BITS;
-        uint64_t acc = 0;
-        for (int d = BITS; d--;) {
-            int lc = fid[d].rank(true, l);
-            int rc = fid[d].rank(true, r);
-            int zero = (r - l) - (rc - lc);
-            bool p = (k >= zero);
-            if (p) {
-                acc |= 1ull << d;
-                l = lc + zero_count[d];
-                r = rc + zero_count[d];
-                k -= zero;
-            } else {
-                l -= lc;
-                r -= rc;
-            }
-        }
-        return acc;
-    }
-    int range_frequency(int l, int r, uint64_t value_l, uint64_t value_r) const {
-        assert(0 <= l and l <= r and r <= fid[0].n);
-        assert(0 <= value_l and value_l <= value_r and value_r <= (1ull << BITS));
-        return range_frequency(BITS - 1, l, r, 0, value_l, value_r);
+    size_t rank(size_t l, size_t r, bool value) const {
+        return value ? rank(r) - rank(l) : (r - l) - (rank(r) - rank(l));
     }
 
 private:
-    int range_frequency(int k, int l, int r, uint64_t v, uint64_t a, uint64_t b) const {
-        if (l == r) return 0;
-        if (k == -1) return (a <= v and v < b) ? r - l : 0;
-        uint64_t nv = v | (1ull << k);
-        uint64_t nnv = nv | ((1ull << k) - 1);
-        if (nnv < a or b <= v) return 0;
-        if (a <= v and nnv < b) return r - l;
-        int lc = fid[k].rank(true, l);
-        int rc = fid[k].rank(true, r);
-        return range_frequency(k - 1, l - lc, r - rc, v, a, b) +
-               range_frequency(k - 1, lc + zero_count[k], rc + zero_count[k], nv, a, b);
+    size_t rank(size_t r) const {
+        return pref[r >> 6] + __builtin_popcountll(bit[r >> 6] & ((static_cast<block_t>(1) << (r & 63)) - 1));
+    }
+};
+
+template <typename T>
+class wavelet_matrix {
+    const size_t size, height;
+    vector<bit_array> bit_matrix;
+    vector<int> mid_point;
+
+public:
+    wavelet_matrix(vector<T> v) : size(v.size()), height(1 + __lg(*max_element(v.begin(), v.end()))),
+                                  bit_matrix(height, bit_array(size)), mid_point(height) {
+        for (const auto& x : v) {
+            assert(x >= 0);
+        }
+        for (auto h = height; h--;) {
+            for (size_t i = 0; i != size; ++i) {
+                bit_matrix[h].set(i, v[i] >> h & 1);
+            }
+            bit_matrix[h].build();
+            mid_point[h] = stable_partition(v.begin(), v.end(), [&](const auto& x) {
+                return !(x >> h & 1);
+            }) - v.begin();
+        }
+    }
+    // # of x in [0, r)
+    size_t rank(size_t r, T x) const {
+        size_t l = 0;
+        for (size_t h = height; h--;) {
+            shrink(l, r, h, x >> h & 1);
+        }
+        return r - l;
+    }
+    // k-th (0-based) smallest value in [l, r)
+    T kth_smallest(size_t l, size_t r, size_t k) const {
+        size_t ret = 0;
+        for (size_t h = height; h--;) {
+            const auto zeros = bit_matrix[h].rank(r, false) - bit_matrix[h].rank(l, false);
+            const auto value = zeros <= k;
+            if (value) {
+                ret |= static_cast<T>(1) << h;
+                k -= zeros;
+            }
+            shrink(l, r, h, value);
+        }
+        return ret;
+    }
+    // k-th (0-based) largest value in [l, r)
+    T kth_largest(size_t l, size_t r, size_t k) const {
+        return kth_smallest(l, r, r - l - k - 1);
+    }
+
+private:
+    void shrink(size_t& l, size_t& r, size_t h, bool value) const {
+        l = bit_matrix[h].rank(l, value) + mid_point[h] * value;
+        r = bit_matrix[h].rank(r, value) + mid_point[h] * value;
+    }
+};
+
+template <typename T>
+class compressed_wavelet_matrix {
+    vector<T> sorted_v;
+    wavelet_matrix<T> wm;
+
+public:
+    compressed_wavelet_matrix(vector<T> v) : sorted_v(v), wm(move(compress(move(v)))) {}
+
+    size_t rank(size_t r, T x) const {
+        return wm.rank(r, get_index(x));
+    }
+    T kth_smallest(size_t l, size_t r, size_t k) const {
+        return sorted_v[wm.kth_smallest(l, r, k)];
+    }
+    T kth_largest(size_t l, size_t r, size_t k) const {
+        return sorted_v[wm.kth_largeest(l, r, k)];
+    }
+
+private:
+    int get_index(T value) {
+        auto it = lower_bound(sorted_v.begin(), sorted_v.end(), value);
+        assert(it != sorted_v.end() && !(value < *it));
+        return it - sorted_v.begin();
+    }
+    vector<int> compress(const vector<T> v) {
+        sort(sorted_v.begin(), sorted_v.end());
+        sorted_v.erase(unique(sorted_v.begin(), sorted_v.end()), sorted_v.end());
+        vector<int> index(v.size());
+        transform(v.begin(), v.end(), index.begin(), [&](const auto& x) {
+            return lower_bound(sorted_v.begin(), sorted_v.end(), x) - sorted_v.begin();
+        });
+        return index;
     }
 };
