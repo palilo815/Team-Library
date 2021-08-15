821ab3921257405b0aec1088fd9a05692db58ada
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-08-15 16:00:17 +0900

    rename "fenwick" -> "binary indexed tree"

--- data/binary_indexed_tree.cpp
+++ data/binary_indexed_tree.cpp
@@ -8,41 +8,46 @@
  *          @see http://boj.kr/2761f0417b9e4126bfd4a02962fbe6dd
  */
 template <typename T>
-class BIT {
-    const int n;
+class binary_indexed_tree {
+    const size_t n;
     vector<T> tree;
 
 public:
-    BIT(int _n) : n(_n), tree(n + 1) {}
-    void update(int i, T val) {
+    binary_indexed_tree(size_t n) : n(n), tree(n + 1) {}
+
+    // a[i] += val
+    void update(size_t i, T val) {
         assert(0 <= i and i <= n);
         for (++i; i <= n; i += i & -i)
             tree[i] += val;
     }
-    T query(int i) const {
+    // sum in range [0...i)
+    T query(size_t i) const {
         assert(0 <= i and i <= n);
         T ret = 0;
         for (; i; i &= i - 1)
             ret += tree[i];
         return ret;
     }
-    T query(int l, int r) const {
+    // sum in range [l, r)
+    T query(size_t l, size_t r) const {
         return query(r) - query(l);
     }
-    T get(int i) const {
+    // a[i]
+    T get(size_t i) const {
         assert(0 <= i and i < n);
         return i & 1 ? query(i, i + 1) : tree[i + 1];
     }
-    int lower_bound(T k) const {
-        int x = 0;
-        for (int pw = 1 << __lg(n); pw; pw >>= 1)
+    size_t lower_bound(T k) const {
+        size_t x = 0;
+        for (size_t pw = 1 << __lg(n); pw; pw >>= 1)
             if ((x | pw) <= n && tree[x | pw] < k)
                 k -= tree[x |= pw];
         return x;
     }
-    int upper_bound(T k) const {
-        int x = 0;
-        for (int pw = 1 << __lg(n); pw; pw >>= 1)
+    size_t upper_bound(T k) const {
+        size_t x = 0;
+        for (size_t pw = 1 << __lg(n); pw; pw >>= 1)
             if ((x | pw) <= n && tree[x | pw] <= k)
                 k -= tree[x |= pw];
         return x;
