5393550bb2f990654b9c2c9f6563e74f370a4530
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-08-21 15:40:54 +0900

    fix doxygen

--- data/binary_indexed_tree.cpp
+++ data/binary_indexed_tree.cpp
@@ -1,11 +1,7 @@
 /**
  * @author  palilo
- * @brief   binary indexed tree (a.k.a. fenwick tree)  
- * @details `lower_bound(k)`
- *              @return minimum i s.t. sum[0...i] >= k
- *          `upper_bound(k)`
- *              @return minimum i s.t. sum[0...i] > k
- *          @see http://boj.kr/2761f0417b9e4126bfd4a02962fbe6dd
+ * @brief   binary indexed tree (a.k.a. fenwick tree)
+ * @example http://boj.kr/2761f0417b9e4126bfd4a02962fbe6dd
  */
 template <typename T>
 class binary_indexed_tree {
@@ -21,7 +17,7 @@ public:
         for (++i; i <= n; i += i & -i)
             tree[i] += val;
     }
-    // sum in range [0...i)
+    // return the sum of the range [0, i)
     T query(size_t i) const {
         assert(0 <= i and i <= n);
         T ret = 0;
@@ -29,15 +25,16 @@ public:
             ret += tree[i];
         return ret;
     }
-    // sum in range [l, r)
+    // return the sum of the range [l, r)
     T query(size_t l, size_t r) const {
         return query(r) - query(l);
     }
-    // a[i]
+    // return a[i]
     T get(size_t i) const {
         assert(0 <= i and i < n);
         return i & 1 ? query(i, i + 1) : tree[i + 1];
     }
+    // return minimum i s.t. sum[0...i] >= k
     size_t lower_bound(T k) const {
         size_t x = 0;
         for (size_t pw = 1 << __lg(n); pw; pw >>= 1)
@@ -45,6 +42,7 @@ public:
                 k -= tree[x |= pw];
         return x;
     }
+    // return minimum i s.t. sum[0...i] > k
     size_t upper_bound(T k) const {
         size_t x = 0;
         for (size_t pw = 1 << __lg(n); pw; pw >>= 1)
