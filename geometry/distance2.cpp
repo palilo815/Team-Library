393378bcf4085bb480abc072481ef669fe3b1b54
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-08-21 15:40:13 +0900

    clean up, remove modulo

--- geometry/distance2.cpp
+++ geometry/distance2.cpp
@@ -1,26 +1,26 @@
 /**
- * @brief
- *      calculate the distance^2 of two objects
- *      return types may be various
+ * @brief calculate the distance^2 of two objects
+ *        return types may be various
  */
 namespace geo::distance2 {
 /**
- * @link 
- *      https://codeforces.com/blog/entry/48868
- * @warning
- *      ccw, no duplicate/collinear points
- * @return
- *      maximum distance of two points on the convex hull
+ * @link    https://codeforces.com/blog/entry/48868
+ * @brief   rotating calipers
+ * @return  maximum distance of two points on the convex hull
+ * @warning ccw order, no duplicate/collinear points
  */
 template <typename T>
 T hull_diameter(const vector<point2D<T>>& hull) {
+    if (hull.size() < 2) return 0;
     T diameter = 0;
-    int n = hull.size();
-    for (int i = 0, j = n > 1; i < j; ++i) {
-        for (;; j = j == n - 1 ? 0 : j + 1) {
+    const size_t n = hull.size();
+    for (size_t i = 0, j = 1, j2; i < j; ++i) {
+        for (;; j = j2) {
+            j2 = j == n - 1 ? 0 : j + 1;
             chmax(diameter, (hull[i] - hull[j]).dist2());
-            if ((hull[(j + 1) % n] - hull[j]).cross(hull[i + 1] - hull[i]) >= 0)
+            if ((hull[j2] - hull[j]).cross(hull[i + 1] - hull[i]) >= 0) {
                 break;
+            }
         }
     }
     return diameter;
