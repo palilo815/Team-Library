86ad2d0ac74fe947341b4b97278898a7efb8911a
palilo815 <hyunsoo1397@gmail.com>
2022-06-29 21:31:04 +0900

    todo: doxygen

--- data/seg_full.cpp
+++ data/seg_full.cpp
@@ -1,61 +1,46 @@
 /**
  * @author  palilo 
- * @brief   segment tree (full binary tree, i.e. # of nodes = 2^k-1)
- * @warning be careful for setting value `e`, it will used for... 
- *          1. dummy nodes (out of range)
- *          2. initial value in `prod` and `op`
- * @todo    e  <- identity element
- *          op <- unite two nodes
+ *
+ *
+ * segment tree (full binary tree, i.e. # of nodes = 2^k-1)
  */
-template <typename node_t>
-class segtree {
-    // change this (1/1)
-    const node_t e {};
-    // change this (1/1)
-    const size_t n, height, size;
+template <class T, class F>
+class SegTree {
+    const size_t n;
+    const T e;
+    const F f;
+    const size_t size;
+    std::vector<T> tree;
 
 public:
-    segtree(size_t n) : n(n), height(n ? __lg(n - 1) + 1 : 0), size(1 << height), tree(size << 1, e) {}
+    SegTree(size_t n, T e, F f) : n(n), e(e), f(f), size(1 << (n ? std::__lg(n - 1) + 1 : 0)), tree(size << 1, e) {}
 
-    node_t& operator[](size_t i) { return tree[size + i]; }
-    void build() {
+    auto operator[](size_t i) -> T& { return tree[size | i]; }
+
+    auto build() {
         for (size_t i = size; i--;) {
             pull(i);
         }
     }
-    void set(size_t idx, node_t val) {
-        assert(0 <= idx and idx < n);
-        tree[idx += size] = val;
-        while (idx >>= 1) pull(idx);
+
+    auto set(size_t i, T val) {
+        assert(i < n);
+        tree[i |= size] = val;
+        while (i >>= 1) pull(i);
     }
-    node_t prod(size_t l, size_t r) const {
-        assert(0 <= l and l <= r and r <= n);
-        node_t lval = e, rval = e;
+
+    [[nodiscard]] auto prod(size_t l, size_t r) const {
+        assert(l <= r and r <= n);
+        T lval = e, rval = e;
         for (l += size, r += size; l != r; l >>= 1, r >>= 1) {
-            if (l & 1) lval = op(lval, tree[l++]);
-            if (r & 1) rval = op(tree[--r], rval);
+            if (l & 1) lval = f(lval, tree[l++]);
+            if (r & 1) rval = f(tree[--r], rval);
         }
-        return op(lval, rval);
+        return f(lval, rval);
     }
 
 private:
-    inline int get_index(node_t& node) const { return &node - tree.data(); }
-    inline int get_depth(node_t& node) const { return __lg(get_index(node)); }
-    inline int get_height(node_t& node) const { return height - get_depth(node); }
-    inline int get_length(node_t& node) const { return 1 << get_height(node); }
-    inline int get_first(node_t& node) const {
-        int idx = get_index(node);
-        int dep = __lg(idx);
-        int len = 1 << height - dep;
-        return len * (idx ^ 1 << dep);
+    auto pull(size_t i) {
+        tree[i] = f(tree[i << 1], tree[i << 1 | 1]);
     }
-    void pull(size_t i) {
-        tree[i] = op(tree[i << 1], tree[i << 1 | 1]);
-    }
-    // change this (2/2)
-    node_t op(node_t lhs, node_t rhs) const {
-        assert(false);
-        return node_t {};
-    }
-    // change this (2/2)
 };
