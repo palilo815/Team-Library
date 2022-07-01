3693c7e8e62e78709108639950147d682a641cd4
palilo815 <hyunsoo1397@gmail.com>
2022-07-02 00:07:17 +0900

    todo: doxygen

--- data/seg_lazy.cpp
+++ data/seg_lazy.cpp
@@ -1,102 +1,74 @@
 /**
  * @author  palilo
  * @brief   segment tree with lazy propagation
- * @todo    `e` and `off`        <- identity element
- *          `op`                 <- unite two nodes
- *          `mapping`            <- apply tag to node
- *          `composition`        <- unite two tags
- * @warning be careful for setting value `e`, it will used for...
- *          1. dummy nodes (out of range)
- *          2. initial value in `prod` and `op`
- *          cannot use `bool` as `node_t` or `tag_t` (i use l-value reference)
  */
-template <typename node_t, typename tag_t>
-class lazy_segtree {
-    // change this (1/2)
-    const node_t e {};
-    const tag_t off {};
-    // change this (1/2)
-    const size_t n, height, size;
-    vector<node_t> tree;
-    vector<tag_t> lazy;
+template <typename T, typename U, typename F0, typename F1, typename F2>
+class LazySeg {
+    const size_t n;
+    const T e;
+    const U off;
+    const F0 op;
+    const F1 mapping;
+    const F2 composition;
+    const size_t size;
+    std::vector<T> tree;
+    std::vector<U> lazy;
 
 public:
-    lazy_segtree(size_t n) : n(n), height(n ? __lg(n - 1) + 1 : 0), size(1 << height),
-                             tree(size << 1, e), lazy(size, off) {}
+    LazySeg(size_t n, T e, U off, F0 op, F1 mapping, F2 composition)
+        : n(n), e(e), off(off), op(op), mapping(mapping), composition(composition),
+          size(1 << (n ? std::__lg(n - 1) + 1 : 0)), tree(size << 1, e), lazy(size, off) {}
 
-    node_t& operator[](size_t i) { return tree[size + i]; }
-    void build() {
-        for (size_t i = size; i--;) {
+    auto operator[](size_t i) -> T& { return tree[size | i]; }
+
+    auto build() {
+        for (auto i = size; i--;) {
             pull(i);
         }
     }
-    void apply(size_t l, size_t r, tag_t f) {
+    auto apply(size_t l, size_t r, U f) {
         apply(l, r, f, 0, size, 1);
     }
-    node_t prod(size_t l, size_t r) {
+    auto prod(size_t l, size_t r) {
         return prod(l, r, 0, size, 1);
     }
-    node_t all_prod() const {
+    [[nodiscard]] auto all_prod() const {
         return tree[1];
     }
 
 private:
 #define lson (i << 1)
 #define rson (i << 1 | 1)
-    inline int get_index(node_t& node) const { return &node - tree.data(); }
-    inline int get_depth(node_t& node) const { return __lg(get_index(node)); }
-    inline int get_height(node_t& node) const { return height - get_depth(node); }
-    inline int get_length(node_t& node) const { return 1 << get_height(node); }
-    inline int get_first(node_t& node) const {
-        int idx = get_index(node);
-        int dep = __lg(idx);
-        int len = 1 << height - dep;
-        return len * (idx ^ 1 << dep);
-    }
-    void apply(size_t ql, size_t qr, tag_t f, size_t l, size_t r, size_t i) {
+    auto apply(size_t ql, size_t qr, U f, size_t l, size_t r, size_t i) {
         if (qr <= l || r <= ql) return;
         if (ql <= l && r <= qr) {
             all_apply(i, f);
             return;
         }
         if (lazy[i] != off) push(i);
-
         const auto m = (l + r) >> 1;
         apply(ql, qr, f, l, m, lson), apply(ql, qr, f, m, r, rson);
         pull(i);
     }
-    node_t prod(size_t ql, size_t qr, size_t l, size_t r, size_t i) {
+    auto prod(size_t ql, size_t qr, size_t l, size_t r, size_t i) {
         if (qr <= l || r <= ql) return e;
         if (ql <= l && r <= qr) return tree[i];
         if (lazy[i] != off) push(i);
-
         const auto m = (l + r) >> 1;
         return op(prod(ql, qr, l, m, lson), prod(ql, qr, m, r, rson));
     }
-    void pull(size_t i) {
+    auto pull(size_t i) {
         tree[i] = op(tree[lson], tree[rson]);
     }
-    void push(size_t i) {
+    auto push(size_t i) {
         all_apply(lson, lazy[i]);
         all_apply(rson, lazy[i]);
         lazy[i] = off;
     }
-    void all_apply(size_t i, tag_t f) {
+    auto all_apply(size_t i, const U& f) {
         mapping(tree[i], f);
         if (i < size) composition(lazy[i], f);
     }
-    // change this (2/2)
-    node_t op(node_t lhs, node_t rhs) const {
-        assert(false);
-        return node_t {};
-    }
-    void mapping(node_t& node, tag_t f) {
-        assert(false);
-    }
-    void composition(tag_t& tag, tag_t f) {
-        assert(false);
-    }
-    // change this (2/2)
 #undef lson
 #undef rson
 };
